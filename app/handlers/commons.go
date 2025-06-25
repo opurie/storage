cb075a9e7c2d80fc4be7c636d32197b2574bed9c
opurie <janpietrzak00@gmail.com>
2025-06-25 18:56:00 +0200

    add commons, responds

---
+++ app/handlers/commons.go
@@ -0,0 +1,21 @@
+package handlers
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func respondJson(w http.ResponseWriter, status int, payload interface{}){
+	response, err := json.Marshal(payload)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write(response)
+}
+
+func respondError(w http.ResponseWriter, status int, message string) {
+	respondJson(w, status, map[string]string{"error": message})
+}
