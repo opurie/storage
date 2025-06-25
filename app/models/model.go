8153f8d7447fdda2b5e96edcf169442f30be3393
opurie <janpietrzak00@gmail.com>
2025-06-25 18:42:26 +0200

    add model

--- app/models/model.go
+++ app/models/model.go
@@ -1,9 +1,14 @@
 package model
 
+import (
+	"github.com/jinzhu/gorm"
+)
+
 type User struct {
 	ID       int    `json:"id"`
 	Username string `json:"username"`
 	Email    string `json:"email"`
+	Description string `json:"description"`
 }
 
 type Item struct {
@@ -13,7 +18,13 @@ type Item struct {
 	Location Location
 	Owner User
 	Description string
-	Tags []Tag
+	Tags []string
+}
+
+type Image struct {
+	ID int
+	ItemID int
+	Path string
 }
 
 type Category struct {
@@ -25,6 +36,20 @@ type Location struct {
 	Name string
 }
 
-type Tag struct{
-	Name string
+
+func DBMigrate(db *gorm.DB) *gorm.DB {
+	db.AutoMigrate(&User{})
+	db.AutoMigrate(&Item{})
+	db.AutoMigrate(&Category{})
+	db.AutoMigrate(&Location{})
+	db.AutoMigrate(&Image{})
+
+	// Add foreign keys
+	db.Model(&Item{}).AddForeignKey("category_id", "categories(id)", "RESTRICT", "RESTRICT")
+	db.Model(&Item{}).AddForeignKey("location_id", "locations(id)", "RESTRICT", "RESTRICT")
+	db.Model(&Item{}).AddForeignKey("owner_id", "users(id)", "RESTRICT", "RESTRICT")
+	db.Model(&Image{}).AddForeignKey("item_id", "items(id)", "RESTRICT", "RESTRICT")	
+	// Add secondary keys
+	db.Model(&User{}).AddUniqueIndex("idx_username", "username")
+	return db
 }
